345eacc70e04762903df2d1665b7f4b8dc8798e8
Oleh Feilo <oleh@i.ua>
2020-07-02 23:56:31 +0300

    add GDT

--- start.s
+++ start.s
@@ -12,6 +12,35 @@
     .long MB_FLAGS
     .long MB_CHECKSUM
 
+.section .gdt
+    gdt_start:
+
+    gdt_null:
+      .long 0x0
+      .long 0x0
+
+    gdt_code:
+      .word 0xffff     # limit 0-15
+      .word 0x0        # base 0-15
+      .byte 0x0        # base 16-23
+      .byte 0b10011010 # flags, type
+      .byte 0b11001111 # flags, limit 16-19
+      .byte 0x0        # base 24-31
+
+    gdt_data:
+      .word 0xffff     # limit 0-15
+      .word 0x0        # base 0-15
+      .byte 0x0        # base 16-23
+      .byte 0b10010010 # flags, type
+      .byte 0b11001111 # flags, limit 16-19
+      .byte 0x0        # base 24-31
+
+    gdt_end:
+
+    gdt_descriptor:
+      .word gdt_end - gdt_start - 1
+      .word gdt_start
+
 .section .bss
     .align 16
     stack_bottom:
@@ -21,6 +50,8 @@
 .section .text
     start:
         mov $stack_top, %esp
+
+        lgdt (gdt_descriptor)
 
         call kernel_main
 
